371b7f115972d7bc7a24c6dcb7f18edef8600e34
Iurii Rychkov <rychkoviurii@hotmail.com>
2024-11-11 23:10:43 +0200

    Added errors.c

---
+++ errors.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   errors.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/11 22:47:05 by irychkov          #+#    #+#             */
+/*   Updated: 2024/11/11 22:56:10 by irychkov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	manual(void)
+{
+	write(1, "Usage ./philo number_of_philosophers time_to_die time_to_eat \
+time_to_sleep [number_of_times_each_philosopher_must_eat]\n", 119);
+	return (0);
+}
+
+void	free_all(t_program_data *data, t_single_philo *philos)
+{
+	free(data->forks);
+	free(data);
+	free(philos);
+}
+
+void	destroy_mutexes(t_program_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (i < data->number_of_philosophers)
+	{
+		pthread_mutex_destroy(&data->forks[i]);
+		i++;
+	}
+	pthread_mutex_destroy(&data->mutex_print);
+}
