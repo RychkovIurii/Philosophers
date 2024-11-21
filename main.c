57b63458368c72666b966368d51fbd60ab7d8d46
Rychkov Iurii <irychkov@student.hive.fi>
2024-11-21 14:35:28 +0200

    Add thinking for one philo

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 14:18:39 by irychkov          #+#    #+#             */
-/*   Updated: 2024/11/21 13:39:24 by irychkov         ###   ########.fr       */
+/*   Updated: 2024/11/21 14:31:32 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ static void	*routine(void *arg)
 	pthread_mutex_unlock(&philo->data->mutex_main);
 	if (philo->number_of_philosophers == 1)
 	{
+		print_msg(philo->data, philo->id, 4, philo->start_time);
 		pthread_mutex_lock(philo->left_fork);
 		print_msg(philo->data, philo->id, 1, philo->start_time);
 		custom_wait(philo, philo->data->time_to_die + 1);
